330fac9c6e1e3d8affff1f59bdf69034a64e1ed3
zachbuchanan <helloworldksu@4261811f904c>
2019-10-03 21:49:22 +0000

    added rederect func

---
+++ .bash_history
@@ -0,0 +1,16 @@
+git status
+git init
+git add .
+git commit -m "first commit"
+ls
+cd mysite/
+ls
+cd ..
+git config --global username "zachbuchanan"
+git config --global user.name "zachbuchanan"
+git commit -m "first commit"
+git remote add origin https://github.com/HelloWorldKSU/Reminder-App.git
+git remote -v
+git push -u origin master
+git pull
+exit
--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -7,13 +7,22 @@ reminders = []
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', reminders = reminders )
+    #return render_template('index.html', reminders = reminders )
+    return render_template('index.html')
+
 
 @app.route('/createItem', methods = ['POST'])
 def createNote():
     note = request.form['reminder']
     reminders.append(note)
-    return redirect('/')
-    
+    return redirect('/returnIndex')
+
+@app.route('/returnIndex')
+def returnIndex():
+    if(len(reminders)):
+        return render_template('index.html', reminders = reminders)
+    else:
+        return render_template('index.html')
+
 if __name__ == "__main__":
     app.run()
